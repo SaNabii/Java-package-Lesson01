f4c532d128a2031a7f8dc008dc92513dca541b8d
Aleksandr <sanabii952@gmail.com>
2021-06-08 12:06:08 +0300

    FirstStep

---
+++ src/Lesson01/FirstClass.java
@@ -0,0 +1,10 @@
+package Lesson01;
+
+public class FirstClass {
+
+    public static void main (String[] args) {
+        System.out.println("Privet ");
+        System.out.println("Privet Mir");
+    }
+
+}
